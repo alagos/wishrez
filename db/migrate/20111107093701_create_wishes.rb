9fc98d6429c35da76f25b5463b96cbb6a1316193
Alter Lagos <alter.strife@gmail.com>
2011-11-07 23:42:53 +1300

    mistake corrected

--- db/migrate/20111107093701_create_wishes.rb
+++ db/migrate/20111107093701_create_wishes.rb
@@ -2,7 +2,7 @@ class CreateWishes < ActiveRecord::Migration
   def self.up
     create_table :wishes do |t|
       t.string :description
-      t.number :priority
+      t.integer :priority
       t.date :arrival
       t.references :status
 
