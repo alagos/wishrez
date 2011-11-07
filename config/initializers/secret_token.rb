3ccc64ddf2b7d9545fec94325277f970e8661bd6
Alter Lagos <alter.strife@gmail.com>
2011-11-07 18:48:30 +1300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wishrez::Application.config.secret_token = 'fb1b49344c584026fd4532e8d34c497272dfa5a3f325afd81b08a93da7f3f04d1f7bb2c4e8f467bc11efe4460f389e015cde2a771281361232973d7e607438b8'
